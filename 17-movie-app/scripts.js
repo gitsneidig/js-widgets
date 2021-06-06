781c2e12dcf1618efe89863051fee52830b33e5d
gitsneidig <scott@scottneidig.com>
2021-06-06 16:56:24 -0500

    Update to Movie App project

--- 17-movie-app/scripts.js
+++ 17-movie-app/scripts.js
@@ -1,5 +1,5 @@
 // The value of API_KEY should come from a key obtained at https://www.themoviedb.org/settings/api
-const API_KEY = ''
+const API_KEY = '8e67679199523163c89c0ce20ff35a61'
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&api_key=${API_KEY}`
 const API_IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const API_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=8e67679199523163c89c0ce20ff35a61&query="'
